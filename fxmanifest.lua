300514284f68f45b2886b213eeefa89b232611a0
fvckdaopps <166699959+fvckdaopps@users.noreply.github.com>
2024-04-11 10:32:30 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'fvckdaopps'
+
+description 'handlings-fivem'
+
+files {
+    'stream/****/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'stream/****/handling.meta'
